19c5c44081559b7e5795b27b9c20d73877edeff0
PCSSW <avieiraborges2005@gmail.com>
2023-09-05 14:35:27 -0300

    Quarto commit: model/migration Catador

--- app/Models/Disponibilidade.php
+++ app/Models/Disponibilidade.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Disponibilidades extends Model
+class Disponibilidade extends Model
 {
     use HasFactory;
 }
